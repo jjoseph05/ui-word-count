bc29fb5d0fd4c3da7c131164790d9a8e98fca574
Justin Joseph <justinjoseph1@gmail.com>
2019-02-08 17:50:10 -0600

    Initial commit, bootstrapped with webpack, babel, react & react-dom

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+}
