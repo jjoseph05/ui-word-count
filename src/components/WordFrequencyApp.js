710f0aecc0ba37a4b45b40a35e7c7c75e9b63697
Justin Joseph <justinjoseph1@gmail.com>
2019-02-10 13:32:07 -0600

    Basic static app established...after some rubber ducking, pivoting to redux for using store instead of local storage for persisting data

--- src/components/WordFrequencyApp.js
+++ src/components/WordFrequencyApp.js
@@ -1,11 +1,60 @@
 import React from 'react';
 
-export default class IndecisionApp extends React.Component {
+export default class WordFrequencyApp extends React.Component {
   render() {
+    const words = [ { 'hello' : 1 }, { 'yellow' : 2 }, { 'poop' : 21 } ];
     return (
       <div>
-        <p>Entry Point for Frequency Analysis</p>
+        <ContentInputContainer />
+        <FrequencyCountList words={words}/>
       </div>
     )
   }
 }
+
+class ContentInputContainer extends React.Component {
+  handleAnalyzeSumbit(e) {
+    e.preventDefault();
+    const contentForAnalysis = e.target.elements.wordContent.value;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleAnalyzeSumbit}>
+        <textarea rows="10" cols="50" name="wordContent"></textarea>
+        <button>Analyze</button>
+      </form>
+    )
+  }
+}
+
+class FrequencyCountList extends React.Component {
+  render() {
+    const words = [ { 'hello' : 1 }, { 'yellow' : 2 }, { 'poop' : 21 } ];
+    return (
+      <div>
+      <p>Word : Frequency</p>
+      {words.length}
+      {
+        words.map((word, index)=> (
+          Object.keys(word).map((key) => (
+            <FrequencyCountItem
+              key={index}
+              word={key}
+              count={word[key]}
+          />
+          ))
+        ))
+      }
+
+    </div>
+    )
+  }
+}
+
+const FrequencyCountItem = (props) => (
+  <div>
+    {console.log(props)}
+    <p>{props.word} : {props.count}</p>
+  </div>
+);
