bc7fc561327e6c6c9ecef2b7a5665236d8e69075
Justin Joseph <justinjoseph1@gmail.com>
2019-02-11 02:50:44 -0600

    User data can be analyzed for frequency of words, and displyed below, visuals need love

--- src/components/WordFrequencyApp.js
+++ src/components/WordFrequencyApp.js
@@ -1,60 +1,105 @@
 import React from 'react';
+import analyzeText from '../helpers/analyzeText'
 
 export default class WordFrequencyApp extends React.Component {
+  state = {
+    inputData: {}
+  };
+
+  handleUserSubmit = (e) => {
+    e.preventDefault();
+    const userInput = analyzeText(e.target.elements.wordContent.value);
+    this.setState(() => ({ inputData: userInput }));
+  }
+
   render() {
-    const words = [ { 'hello' : 1 }, { 'yellow' : 2 }, { 'poop' : 21 } ];
+    const words = this.state.inputData;
+
     return (
       <div>
-        <ContentInputContainer />
+        <form onSubmit={this.handleUserSubmit}>
+          <textarea rows="10" cols="50" name="wordContent"></textarea>
+          <button>Analyze</button>
+        </form>
         <FrequencyCountList words={words}/>
       </div>
     )
   }
 }
 
-class ContentInputContainer extends React.Component {
-  handleAnalyzeSumbit(e) {
-    e.preventDefault();
-    const contentForAnalysis = e.target.elements.wordContent.value;
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleAnalyzeSumbit}>
-        <textarea rows="10" cols="50" name="wordContent"></textarea>
-        <button>Analyze</button>
-      </form>
-    )
-  }
-}
-
-class FrequencyCountList extends React.Component {
-  render() {
-    const words = [ { 'hello' : 1 }, { 'yellow' : 2 }, { 'poop' : 21 } ];
-    return (
-      <div>
+const FrequencyCountList = (props) => {
+  return ( 
+    <div>
       <p>Word : Frequency</p>
-      {words.length}
-      {
-        words.map((word, index)=> (
-          Object.keys(word).map((key) => (
-            <FrequencyCountItem
-              key={index}
-              word={key}
-              count={word[key]}
+      {console.log(Object.keys(props.words).length)}
+      {props.words &&
+        Object.keys(props.words).map((word, index) => (
+          <FrequencyCountItem
+            key={index}
+            word={word}
+            count={props.words[word]}
           />
-          ))
         ))
       }
-
     </div>
-    )
-  }
+  )
 }
 
 const FrequencyCountItem = (props) => (
-  <div>
-    {console.log(props)}
-    <p>{props.word} : {props.count}</p>
-  </div>
+  <span> &#123; {props.word} : {props.count} &#125;</span>
 );
+
+// props.inputData.map((word, index)=> (
+//   Object.keys(word).map((key) => (
+//     <FrequencyCountItem
+//       key={index}
+//       word={key}
+//       count={word[key]}
+//   />
+//   ))
+// ))
+
+
+// class FrequencyCountList extends React.Component {
+//   render() {
+//     console.log(this.state, 'hello');
+//     const words = [ { 'hello' : 1 }, { 'yellow' : 2 }, { 'poop' : 21 } ];
+//     return (
+//       <div>
+//       <p>Word : Frequency</p>
+//       {words.length}
+//       {
+//         words.map((word, index)=> (
+//           Object.keys(word).map((key) => (
+//             <FrequencyCountItem
+//               key={index}
+//               word={key}
+//               count={word[key]}
+//           />
+//           ))
+//         ))
+//       }
+
+//     </div>
+//     )
+//   }
+// }
+
+
+
+
+// class ContentInputContainer extends React.Component {
+//   handleAnalyzeSumbit(e) {
+//     e.preventDefault();
+//     const contentForAnalysis = e.target.elements.wordContent.value;
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleAnalyzeSumbit}>
+//         <textarea rows="10" cols="50" name="wordContent"></textarea>
+//         <button>Analyze</button>
+//       </form>
+//     )
+//   }
+// }
