c13b0bfa76adb3d8c3866037bc25171e0d8726b0
Justin Joseph <justinjoseph1@gmail.com>
2019-02-13 14:56:11 -0600

    Added production flag for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
 const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  entry: './src/App.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, '/dist')
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    entry: './src/App.js',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, '/dist')
+    }
   }
 };
