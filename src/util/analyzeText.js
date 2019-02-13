ce21583720cdc2d16bfdb97d9882df2cab211cf8
Justin Joseph <justinjoseph1@gmail.com>
2019-02-13 11:45:17 -0600

    Added function to remap frequencymap object

--- src/util/analyzeText.js
+++ src/util/analyzeText.js
@@ -1,11 +1,25 @@
 export default function analyzeText(str) {
   const wordsArray = str.split(/\s+/),
         frequencyMap = {};
+
   wordsArray.map((word)=> (
     frequencyMap[word] = frequencyMap[word] + 1 || 1
   ))
-  
-  return frequencyMap;
+  return reMapData(frequencyMap);
+}
+
+function reMapData(data) {
+  let remappedObject = {};
+
+    remappedObject = Object.keys(data).map(function(word, index) {
+      return {
+        name: 'name',
+        word: word,
+        count: data[word],
+        key: index + 1
+      }
+    })
+  return remappedObject;
 }
 // TODO make more robust for none characters e.g. !@#$&*
 // module.exports = analyzeText;
