920152633aa7f3885a08578ed625bb334651eb32
Justin Joseph <justinjoseph1@gmail.com>
2019-02-13 18:31:05 -0600

    Added Header

--- src/components/WordFrequencyApp.js
+++ src/components/WordFrequencyApp.js
@@ -38,7 +38,7 @@ class WordFrequencyApp extends React.Component {
 
 const Header = (props) => (
   <div className="header">
-    <div className="container">
+    <div>
       <h1 className="header__title">{props.title}</h1>
       {props.subTitle && <h2 className="header__subtitle">{props.subTitle}</h2>}
     </div>
