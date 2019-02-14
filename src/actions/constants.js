20fadeab94be9a5b8d7f9f314d4cde99b1cb90a2
Justin Joseph <justinjoseph1@gmail.com>
2019-02-14 08:31:16 -0600

    Updates

--- src/actions/constants.js
+++
@@ -1,3 +0,0 @@
-// Action Constants
-
-export const ADD_USER_INPUT = "ADD_USER_INPUT";
--- src/actions/userInput.js
+++
@@ -1,9 +0,0 @@
-// UserInput Actions
-import { ADD_USER_INPUT } from './constants';
-
-export default (wordCount) => {
-  return {
-    type: ADD_USER_INPUT,
-    payload: wordCount
-  };
-};
