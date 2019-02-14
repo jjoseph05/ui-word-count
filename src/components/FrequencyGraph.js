e2f3296a66a53160405c27d7972a95620dc2eb43
Justin Joseph <justinjoseph1@gmail.com>
2019-02-13 23:00:40 -0600

    Added log scale for yAxis

--- src/components/FrequencyGraph.js
+++ src/components/FrequencyGraph.js
@@ -10,7 +10,7 @@ const FrequencyGraph = (props) => {
               margin={{top: 5, right: 30, left: 20, bottom: 5}}>
           <CartesianGrid strokeDasharray="3 3"/>
           <XAxis dataKey="word" style={{display: "none"}}/>
-          <YAxis/>
+          <YAxis scale="log" domain={['dataMin', 'dataMax']} allowDataOverflow/>
           <Tooltip/>
           <Bar dataKey="count" fill="#5A87A9" />
         </BarChart>
